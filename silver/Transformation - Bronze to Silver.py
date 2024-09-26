d761469725d6556e7d2842c621fba4152c4e3691
yuriralopes <yuriralopes>
2024-09-26 17:11:51 +0000

    Changes in Bronze to Silver Notebook

--- silver/Transformation - Bronze to Silver.py
+++ silver/Transformation - Bronze to Silver.py
@@ -1,2 +1,17 @@
 # Databricks notebook source
+# DBTITLE 1,Mounting Azure Data Lake
+configs = {"fs.azure.account.auth.type": "OAuth",
+          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+          "fs.azure.account.oauth2.client.id": "5d49f494-8b9f-429b-95f0-2d60f9a4f4ea",
+          "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="datalakehouse",key="ADLS-DATABRICKS-KEY"),
+          "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/d1c90e28-32cb-41ac-b3eb-5dced487b30a/oauth2/token"}
 
+# Optionally, you can add <directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = "abfss://bronze@dldatalakehouse.dfs.core.windows.net/",
+  mount_point = "/mnt/bronze/",
+  extra_configs = configs)
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt/bronze/Sales/")
