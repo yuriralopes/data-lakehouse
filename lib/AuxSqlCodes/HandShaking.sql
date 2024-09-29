76e41e120a881cf12afddb465f0728ed133599ab
yuriralopes <yuriralopes@gmail.com>
2024-09-29 14:05:39 -0700

    changing HNDsHAKING

---
+++ lib/AuxSqlCodes/HandShaking.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[HandshakingDB] (
+	
+	[name_Schema] [nvarchar](255) NOT NULL,
+	[name_table] [nvarchar](255) NOT NULL, 
+    [last_execution_date] [nvarchar](8) NOT NULL,
+    [low_last_execution_time] [nvarchar](14) NOT NULL,
+	[high_last_execution_time] [nvarchar](14) NOT NULL,
+
+	
+	)
