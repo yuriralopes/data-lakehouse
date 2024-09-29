bc09bdc06762a8c3be48cbaa2830db341d86a8b3
yuriralopes <yuriralopes>
2024-09-29 21:03:54 +0000

    Change Notebooks

--- gold/Silver to Gold.py
+++ gold/Silver to Gold.py
@@ -1,11 +1,47 @@
 # Databricks notebook source
-dbutils.fs.ls('/mnt/silver/Sales/')
+# MAGIC %md
+# MAGIC
 
 # COMMAND ----------
 
-from pyspark.sql.functions import col
-input_path = '/mnt/silver/Sales/Store/'
+# MAGIC %md
+# MAGIC ## Doing transformation and Ingestion from Silver to Gold Container
 
-df = spark.read.format('delta').load(input_path)
+# COMMAND ----------
 
-display(df.where(col("BusinessEntityID") == 292))
+# DBTITLE 1,Import Libs
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, regexp_replace
+
+# COMMAND ----------
+
+# DBTITLE 1,Getting all tables in the container
+table_name = []
+print(table)
+for i in dbutils.fs.ls("/mnt/silver/Sales/"):
+    table_name.append(i.name.split('/')[0])
+table_name
+
+# COMMAND ----------
+
+# DBTITLE 1,Transformation
+
+for name in table_name:
+	path = f'/mnt/silver/Sales/{name}/'
+	df = spark.read.format('delta').load(path)
+
+	column_names = df.columns
+
+	for old_col_name in column_names:
+		#convert column name from ColumnName to Column_Name format
+		new_col_name = "".join(["_" + char if char.isupper() and not old_col_name[i-1].isupper() else char for i, char in enumerate(old_col_name)]).lstrip("_")
+
+		df = df.withColumnRenamed(old_col_name, new_col_name)
+
+	output_path = f'/mnt/gold/Sales/{name}'
+	df.write.format('delta').mode("overwrite").save(output_path)
+
+# COMMAND ----------
+
+# DBTITLE 1,Checking result for last table
+display(df)
--- silver/Bronze to Silver.py
+++ silver/Bronze to Silver.py
@@ -4,29 +4,91 @@
 
 # COMMAND ----------
 
-# DBTITLE 1,Getting all tables in the container
+# DBTITLE 1,Imports
+import delta
+import pyspark.sql.functions as F
+from pyspark.sql.functions import col, lit, concat
 import os
 import sys
-sys.path.append(os.path.abspath("/data-lakehouse/y-alencar@hotmail.com/data-lakehouse/lib/ingestion"))
+from delta.tables import *
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType
 
+# COMMAND ----------
+
+# DBTITLE 1,Classes and Functions
+class IngestionSilver:
+
+    def __init__(self, table, sourcePath, isDelta):
+        self.table = table
+        self.sourcePath = sourcePath
+        self.isDelta = isDelta
+        self.spark = spark
+        self.output_path = f'/mnt/silver/Sales/{self.table}/'
+        self.idField = "BusinessEntityID"
+
+    def read(self):
+        df = spark.read.format('parquet').load(self.sourcePath)
+        return df
+    
+    def transform(self, df):
+        df.createOrReplaceTempView(self.table)
+        df_transform = spark.sql(f"select * from {self.table}")
+        return df_transform
+
+    def save(self, df):
+        (df.write
+           .mode("overwrite")
+           .format("delta")
+           .option("overwriteSchema", "true")
+           .save(self.output_path)
+           )
+    
+    def upsert(self, df):
+        delta_table = DeltaTable.forPath(self.spark, self.output_path)
+        
+        delta_table.alias("d").merge(df.alias("n"), f"d.{self.idField} = n.{self.idField}").whenMatchedUpdateAll().whenNotMatchedInsertAll().execute()
+        
+    def auto(self):
+        df = self.read()
+        df_transform = self.transform(df)
+
+        if self.isDelta == 'Y':
+            self.upsert(df_transform)
+        else:
+            self.save(df_transform)
+
+    
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Getting all tables in the container
 table_name = []
 print(table)
 for i in dbutils.fs.ls("/mnt/bronze/Sales/"):
     table_name.append(i.name.split('/')[0])
 table_name
 
+
 # COMMAND ----------
 
+# DBTITLE 1,Transformation and Ingestion from Bronze Container to Silver
 from datetime import datetime
 
 date_path = datetime.today().strftime('%Y-%m-%d')
+isFirstLoad = True
 
 for i in table_name:
-    sourcePath = '/mnt/bronze/Sales/' + i + '/delta/' + date_path + '/*'
+    sourcePath = '/mnt/bronze/Sales/' + i + '/delta/' + date_path + '/' + i + '*'
+
+    if i == 'Store':
+        isDelta = 'Y'
+    else:
+        isDelta = 'N'
+    
     try:  
-        ingestion = FullIngestionSilver(i, sourcePath)
+        ingestion = IngestionSilver(i, sourcePath, isDelta)
         ingestion.auto()
         print(f"Loading data from path: {sourcePath}")
     except Exception as e:
         print(f"Path does not exist: {sourcePath}")
-
